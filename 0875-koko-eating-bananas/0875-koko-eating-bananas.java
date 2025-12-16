d362bd3618a196edb1b991442de515adc59eb944
Jinyoung Jeon <83676788+acnove53@users.noreply.github.com>
2025-12-16 14:47:49 -0500

    Time: 40 ms (5.23%), Space: 47.3 MB (73.61%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,18 +1,50 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int speed = 1;
-        while (true) {
-            double time = 0;
+        int up = 0;
+        for (int i = 1; i < piles.length; i++) {
+            if (piles[up] < piles[i]) {
+                up = i;
+            }
+        }
+        up = piles[up];
+        int low = 1;
+        System.out.println(up);
+
+        //Input: piles = [3,6,7,11], h = 8
+        // Output: 4
+        double time = 0;
+        while (low < up) {
+            time = 0;
+            int mid = low + ((up - low) / 2);
             for (int bananas : piles) {
-                time += Math.ceil((double)bananas / speed);
+                time += (int) Math.ceil((double)bananas / mid);
                 if (time > h) {
                     break;
                 }
             }
             if (time <= h) {
-                return speed;
+                up = mid;
+                System.out.println("up=" + up);
+            } else {
+                low = mid + 1;
+                System.out.println("down=" + low);
             }
-            speed ++;
         }
+        return up;
+
+        // int speed = 1;
+        // while (true) {
+        //     double time = 0;
+        //     for (int bananas : piles) {
+        //         time += Math.ceil((double)bananas / speed);
+        //         if (time > h) {
+        //             break;
+        //         }
+        //     }
+        //     if (time <= h) {
+        //         return speed;
+        //     }
+        //     speed ++;
+        // }
     }
 }
