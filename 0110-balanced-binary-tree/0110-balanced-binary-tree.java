23352ebd1b977ae54549124d7a786c77212b4ec8
Jinyoung Jeon <83676788+acnove53@users.noreply.github.com>
2026-01-23 14:32:24 -0500

    Time: 0 ms (100%), Space: 45.4 MB (91.91%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (height(root) == -1) {
+        if (height(root) == Integer.MIN_VALUE) {
             return false;
         }
         return true;
@@ -23,19 +23,19 @@ class Solution {
 
     private int height(TreeNode curr) {
         if (curr == null) {
-            return 0;
+            return -1;
         }
 
         int l = height(curr.left);
-        if (l == -1) {
-            return -1;
+        if (l == Integer.MIN_VALUE) {
+            return l;
         }
         int r = height(curr.right);
-        if (r == -1) {
-            return -1;
+        if (r == Integer.MIN_VALUE) {
+            return r;
         }
         if (Math.abs(l-r) > 1) {
-            return -1;
+            return Integer.MIN_VALUE;
         }
         return Math.max(l,r) + 1;
     }
